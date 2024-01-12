2f0056a5fe04d478250f1da9ad8d065e9cd480de
y-tanaka <y-tanaka>
2024-01-12 18:03:13 +0900

    first commit

---
+++ abc335/src/bin/a.rs
@@ -0,0 +1,7 @@
+use proconio::input;
+
+fn main() {
+    input! {}
+    let mut ans = "";
+    println!("{}", ans);
+}
---
+++ abc335/src/bin/e.rs
@@ -0,0 +1,8 @@
+use proconio::input;
+
+fn main() {
+	input! {
+  }
+	let mut ans = "";
+	println!("{}",ans);
+}
